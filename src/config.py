1ac1406276abc7b995f2d0085848102c80118659
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-01 02:03:06 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -24,14 +24,14 @@ ENC = "50"
 ENC_PRETRAINED = True  # pre-trained on ImageNet or randomly initialised
 
 # GENERAL
-EVALUATE = False
+EVALUATE = True
 FREEZE_BN = [True] * 3
 NUM_SEGM_EPOCHS = [100] * 3
 PRINT_EVERY = 10
 RANDOM_SEED = 42
 CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR/ckpt/'
 VAL_EVERY = [10] * 3  # how often to record validation scores
-RESUME = ''#'/content/light-weight-refinenet/ckpt/'
+RESUME = '/content/drive/MyDrive/Super-BPD/LWR/ckpt/'
 
 # OPTIMISERS' PARAMETERS
 LR_ENC = [5e-4, 2.5e-4, 1e-4]  # TO FREEZE, PUT 0
