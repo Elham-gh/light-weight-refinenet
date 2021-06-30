c4ab583bc3aed800efaf05584df53e3f63c6f855
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-06-30 23:34:42 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -29,10 +29,9 @@ FREEZE_BN = [True] * 3
 NUM_SEGM_EPOCHS = [100] * 3
 PRINT_EVERY = 10
 RANDOM_SEED = 42
-SNAPSHOT_DIR = "./ckpt/"
 CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR/ckpt/'
-VAL_EVERY = [5] * 3  # how often to record validation scores
-RESUME = '/content/light-weight-refinenet/ckpt/'
+VAL_EVERY = [10] * 3  # how often to record validation scores
+RESUME = ''#'/content/light-weight-refinenet/ckpt/'
 
 # OPTIMISERS' PARAMETERS
 LR_ENC = [5e-4, 2.5e-4, 1e-4]  # TO FREEZE, PUT 0
