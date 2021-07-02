5121a9c20215abd3f1f3dbf07271b1d754d3ac3a
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-02 19:17:32 +0430

    SA class

--- models/resnet.py
+++ models/resnet.py
@@ -29,6 +29,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 from utils.helpers import maybe_download
 from utils.layer_factory import conv1x1, conv3x3, CRPBlock
@@ -128,62 +129,43 @@ class Bottleneck(nn.Module):
 
       
 
-class SelfAttention(nn.Module):
-    """
-        Self attention Layer.
-        Source paper: https://arxiv.org/abs/1805.08318
-    """
-    def __init__(self, in_dim, activation=F.relu):
-
-        import torch.nn.Module
-        import torch
-        import torch.nn.init
-
-        super(SelfAttention, self).__init__()
+class Self_Attn(nn.Module):
+    """ Self attention Layer"""
+    def __init__(self, in_dim, activation):
+        super(Self_Attn, self).__init__()
+        
         self.chanel_in = in_dim
         self.activation = activation
         
-        self.f = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8 , kernel_size=1)
-        self.g = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8 , kernel_size=1)
-        self.h = nn.Conv2d(in_channels=in_dim, out_channels=in_dim , kernel_size=1)
-        
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) ###*  out_channels check
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) ###* out_channels check
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         self.gamma = nn.Parameter(torch.zeros(1))
 
-        self.softmax  = nn.Softmax(dim=-1)
-
-        init_conv(self.f)
-        init_conv(self.g)
-        init_conv(self.h)
+        self.softmax  = nn.Softmax(dim=-1) #
         
-
-    def init_conv(conv, glu=True):
-        init.xavier_uniform_(conv.weight)
-        if conv.bias is not None:
-            conv.bias.data.zero_()
-    
-    def forward(self, x):
+    def forward(self,x, b):
         """
             inputs :
-                x : input feature maps( B X C X W X H)
+                x : input feature maps ( B X C X W X H)
+                b : BPD feature maps ( B X C X W X H)
             returns :
-                out : self attention feature maps
-                
+                out : self attention value + input feature 
+                attention: B X N X N (N is Width*Height)
         """
+        
         m_batchsize, C, width, height = x.size()
+        proj_query  = self.query_conv(b).view(m_batchsize,-1,width*height).permute(0,2,1) # B X C X (N) #***
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
+        energy =  torch.bmm(proj_query, proj_key) # transpose check #???
+        attention = self.softmax(energy) # B X (N) X (N) 
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0,2,1))
+        out = out.view(m_batchsize, C, width, height)
         
-        f = self.f(x).view(m_batchsize, -1, width * height) # B * (C//8) * (W * H)
-        g = self.g(x).view(m_batchsize, -1, width * height) # B * (C//8) * (W * H)
-        h = self.h(x).view(m_batchsize, -1, width * height) # B * C * (W * H)
-        
-        attention = torch.bmm(f.permute(0, 2, 1), g) # B * (W * H) * (W * H)
-        attention = self.softmax(attention)
-        
-        self_attetion = torch.bmm(h, attention) # B * C * (W * H)
-        self_attetion = self_attetion.view(m_batchsize, C, width, height) # B * C * W * H
-        
-        out = self.gamma * self_attetion + x
-        return out
-      
+        out = self.gamma * out + x
+        return out, attention      
       
 
 
