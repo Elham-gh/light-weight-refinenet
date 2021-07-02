105c3847b164d5da5562a61e1984580e03f86a33
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-02 19:04:40 +0430

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -126,6 +126,66 @@ class Bottleneck(nn.Module):
 
         return out
 
+      
+
+class SelfAttention(nn.Module):
+    """
+        Self attention Layer.
+        Source paper: https://arxiv.org/abs/1805.08318
+    """
+    def __init__(self, in_dim, activation=F.relu):
+
+        import torch.nn.Module
+        import torch
+        import torch.nn.init
+
+        super(SelfAttention, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+        
+        self.f = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8 , kernel_size=1)
+        self.g = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8 , kernel_size=1)
+        self.h = nn.Conv2d(in_channels=in_dim, out_channels=in_dim , kernel_size=1)
+        
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax  = nn.Softmax(dim=-1)
+
+        init_conv(self.f)
+        init_conv(self.g)
+        init_conv(self.h)
+        
+
+    def init_conv(conv, glu=True):
+        init.xavier_uniform_(conv.weight)
+        if conv.bias is not None:
+            conv.bias.data.zero_()
+    
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X W X H)
+            returns :
+                out : self attention feature maps
+                
+        """
+        m_batchsize, C, width, height = x.size()
+        
+        f = self.f(x).view(m_batchsize, -1, width * height) # B * (C//8) * (W * H)
+        g = self.g(x).view(m_batchsize, -1, width * height) # B * (C//8) * (W * H)
+        h = self.h(x).view(m_batchsize, -1, width * height) # B * C * (W * H)
+        
+        attention = torch.bmm(f.permute(0, 2, 1), g) # B * (W * H) * (W * H)
+        attention = self.softmax(attention)
+        
+        self_attetion = torch.bmm(h, attention) # B * C * (W * H)
+        self_attetion = self_attetion.view(m_batchsize, C, width, height) # B * C * W * H
+        
+        out = self.gamma * self_attetion + x
+        return out
+      
+      
+
 
 class ResNetLW(nn.Module):
     def __init__(self, block, layers, num_classes=21):
