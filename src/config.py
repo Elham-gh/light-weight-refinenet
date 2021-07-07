c31744e4344b4cb93351b6f44cda4cafcecf8d72
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-08 00:42:40 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -27,7 +27,7 @@ ENC_PRETRAINED = True  # pre-trained on ImageNet or randomly initialised
 # GENERAL
 EVALUATE = False
 FREEZE_BN = [True] * 3
-NUM_SEGM_EPOCHS = [150] * 3
+NUM_SEGM_EPOCHS = [100] * 3
 PRINT_EVERY = 10
 RANDOM_SEED = 42
 CKPT_PATH = './'# '/content/drive/MyDrive/Super-BPD/LWR/ckpt4/'
@@ -35,8 +35,9 @@ VAL_EVERY = [10] * 3  # how often to record validation scores
 RESUME = ''#'/content/drive/MyDrive/Super-BPD/LWR/ckpt4/'
 
 # OPTIMISERS' PARAMETERS
-LR_ENC = [5e-3, 2.5e-3, 1e-3]  # TO FREEZE, PUT 0
-LR_DEC = [5e-2, 2.5e-2, 1e-2]
+LR_ENC = [1e-2, 5e-3, 2.5e-3]  # TO FREEZE, PUT 0
+LR_DEC = [1e-1, 5e-2, 2.5e-2]
+
 MOM_ENC = [0.9] * 3  # TO FREEZE, PUT 0
 MOM_DEC = [0.9] * 3
 WD_ENC = [1e-5] * 3  # TO FREEZE, PUT 0
