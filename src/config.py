8b85383cfb520af4e3e00372b04950eb841b86ac
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-08 19:20:10 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -36,10 +36,10 @@ RESUME = '/content/drive/MyDrive/Super-BPD/LWR/lr_ckpt/'
 
 # OPTIMISERS' PARAMETERS
 LR_ENC = [1e-2, 5e-4, 2.5e-3]  # TO FREEZE, PUT 0
-LR_DEC = [1e-1, 5e-4, 2.5e-2]
+LR_DEC = [1e-1, 5e-3, 2.5e-2]
 
-MOM_ENC = [0.9, 0, 0.2]  # TO FREEZE, PUT 0
-MOM_DEC = [0.9, 0, 0.2] 
+MOM_ENC = [0.9, 0.5, 0.2]  # TO FREEZE, PUT 0
+MOM_DEC = [0.9, 0.5, 0.2] 
 WD_ENC = [1e-5] * 3  # TO FREEZE, PUT 0
 WD_DEC = [1e-5] * 3
 OPTIM_DEC = "sgd"
