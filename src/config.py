e1df14827262ba2a6752963f314199cff7bdda20
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-20 01:18:42 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ import numpy as np
 # DATASET PARAMETERS
 TRAIN_DIR = '/content/drive/MyDrive/datasets/nyudv2/'
 VAL_DIR = TRAIN_DIR
-TRAIN_LIST = ['/content/light-weight-refinenet/data/train_CEN.nyu'] * 3
-VAL_LIST = ['/content/light-weight-refinenet/data/val_CEN.nyu'] * 3
-BPD_DIR = ['/content/drive/MyDrive/datasets/nyu/CEN_bpds.pkl'] * 3
+TRAIN_LIST = ['/content/light-weight-refinenet/data/train_CEN.nyu'] * 12
+VAL_LIST = ['/content/light-weight-refinenet/data/val_CEN.nyu'] * 12
+BPD_DIR = ['/content/drive/MyDrive/datasets/nyu/CEN_bpds.pkl'] * 12
 SHORTER_SIDE = [350] * 3
 CROP_SIZE = [500] * 3
 NORMALISE_PARAMS = [
@@ -13,7 +13,8 @@ NORMALISE_PARAMS = [
     np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),  # MEAN
     np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3)),
 ]  # STD
-BATCH_SIZE = [1] * 3
+BATCH_SIZE = [6] * 3
+BATCH_MEAN = 12
 NUM_WORKERS = 4
 NUM_CLASSES = [40] * 3
 LOW_SCALE = [0.5] * 3
@@ -30,16 +31,16 @@ FREEZE_BN = [True] * 3
 NUM_SEGM_EPOCHS = [100] * 3
 PRINT_EVERY = 10
 RANDOM_SEED = 42
-CKPT_PATH = './'# '/content/drive/MyDrive/Super-BPD/LWR/ckpt4/'
+CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR/ckpt_att/'
 VAL_EVERY = [10] * 3  # how often to record validation scores
-RESUME = ''#'/content/drive/MyDrive/Super-BPD/LWR/ckpt4/'
+RESUME = ''#'/content/drive/MyDrive/Super-BPD/LWR/ckpt_att_batch/'
 
 # OPTIMISERS' PARAMETERS
-LR_ENC = [1e-6, 5e-3, 2.5e-3]  # TO FREEZE, PUT 0
-LR_DEC = [1e-5, 5e-2, 2.5e-2]
+LR_ENC = [1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5, 1e-5, 5e-6, 5e-6, 2.5e-6, 2.5e-6, 1e-6]  # TO FREEZE, PUT 0
+LR_DEC = [1e-3, 5e-4, 5e-4, 2.5e-4, 2.5e-4, 1e-4, 1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5]
 
-MOM_ENC = [0] * 3  # TO FREEZE, PUT 0
-MOM_DEC = [0] * 3
-WD_ENC = [1e-5] * 3  # TO FREEZE, PUT 0
-WD_DEC = [1e-5] * 3
+MOM_ENC = [0] * 12  # TO FREEZE, PUT 0
+MOM_DEC = [0] * 12
+WD_ENC = [1e-5] * 12  # TO FREEZE, PUT 0
+WD_DEC = [1e-5] * 12
 OPTIM_DEC = "sgd"
