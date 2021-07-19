f48e45d85e6d3d5785896a89ac3639b714e690ec
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-20 01:37:49 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -13,12 +13,12 @@ NORMALISE_PARAMS = [
     np.array([0.485, 0.456, 0.406]).reshape((1, 1, 3)),  # MEAN
     np.array([0.229, 0.224, 0.225]).reshape((1, 1, 3)),
 ]  # STD
-BATCH_SIZE = [6] * 3
+BATCH_SIZE = [6] * 12
 BATCH_MEAN = 12
 NUM_WORKERS = 4
-NUM_CLASSES = [40] * 3
-LOW_SCALE = [0.5] * 3
-HIGH_SCALE = [2.0] * 3
+NUM_CLASSES = [40] * 12
+LOW_SCALE = [0.5] * 12
+HIGH_SCALE = [2.0] * 12
 IGNORE_LABEL = 255
 
 # ENCODER PARAMETERS
@@ -27,8 +27,8 @@ ENC_PRETRAINED = True  # pre-trained on ImageNet or randomly initialised
 
 # GENERAL
 EVALUATE = False
-FREEZE_BN = [True] * 3
-NUM_SEGM_EPOCHS = [100] * 3
+FREEZE_BN = [True] * 12
+NUM_SEGM_EPOCHS = [30] * 12
 PRINT_EVERY = 10
 RANDOM_SEED = 42
 CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR/ckpt_att/'
@@ -39,8 +39,8 @@ RESUME = ''#'/content/drive/MyDrive/Super-BPD/LWR/ckpt_att_batch/'
 LR_ENC = [1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5, 1e-5, 5e-6, 5e-6, 2.5e-6, 2.5e-6, 1e-6]  # TO FREEZE, PUT 0
 LR_DEC = [1e-3, 5e-4, 5e-4, 2.5e-4, 2.5e-4, 1e-4, 1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5]
 
-MOM_ENC = [0] * 12  # TO FREEZE, PUT 0
-MOM_DEC = [0] * 12
+MOM_ENC = [.9] * 12  # TO FREEZE, PUT 0
+MOM_DEC = [.9] * 12
 WD_ENC = [1e-5] * 12  # TO FREEZE, PUT 0
 WD_DEC = [1e-5] * 12
 OPTIM_DEC = "sgd"
