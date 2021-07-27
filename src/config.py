82d2ded44043a93dffc39d2a30fe45b10ec7f67e
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-07-27 15:35:08 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ NORMALISE_PARAMS = [
 ]  # STD
 BATCH_SIZE = [6] * 12
 NUM_WORKERS = 16
-NUM_CLASSES = [40] * 3
+NUM_CLASSES = [40] * 4
 LOW_SCALE = [0.5] * 12
 HIGH_SCALE = [2.0] * 12
 IGNORE_LABEL = 255
@@ -30,13 +30,13 @@ FREEZE_BN = [True] * 12
 NUM_SEGM_EPOCHS = [100] * 12
 PRINT_EVERY = 10
 RANDOM_SEED = 42
-CKPT_PATH = "/content/drive/MyDrive/Super-BPD/LWR/ckpt_multimodalcat/"
+CKPT_PATH = "/content/drive/MyDrive/Super-BPD/LWR/ckpt_multimodalcat_all/"
 RESUME = '' #CKPT_PATH + "353_19/Copy of "
 VAL_EVERY = [10] * 12  # how often to record validation scores
 
 # OPTIMISERS' PARAMETERS
-LR_ENC = [5e-5, 2.5e-5, 2.5e-5, 1e-5, 1e-5, 5e-6, 5e-6, 2.5e-6, 2.5e-6, 1e-6, 1e-6, 5e-7]  # TO FREEZE, PUT 0
-LR_DEC = [5e-4, 2.5e-4, 2.5e-4, 1e-4, 1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5, 1e-5, 5e-6]
+LR_ENC = [1e-3, 1e-4, 1e-5, .5e-5, 1e-5, 5e-6, 5e-6, 2.5e-6, 2.5e-6, 1e-6, 1e-6, 5e-7]  # TO FREEZE, PUT 0
+LR_DEC = [1e-2, 1e-3, 1e-4, .5e-4, 1e-4, 5e-5, 5e-5, 2.5e-5, 2.5e-5, 1e-5, 1e-5, 5e-6]
 MOM_ENC = [0.9] * 12  # TO FREEZE, PUT 0
 MOM_DEC = [0.9] * 12
 WD_ENC = [1e-5] * 12  # TO FREEZE, PUT 0
