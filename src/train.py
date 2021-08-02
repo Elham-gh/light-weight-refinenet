7fd5e82065da6d46a65db2aff96e9d8e2f1ec4ce
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-08-03 04:23:42 +0430

    Update train.py

--- src/train.py
+++ src/train.py
@@ -430,12 +430,13 @@ def train_segmenter(
         start = time.time()
         input = sample['image']
         bpd = sample['bpd'].unsqueeze(1)
-        depth = sample['depth']
+        depth = sample['depth'].unsqueeze(1)
         target = sample["mask"].cuda()
         input_var = torch.autograd.Variable(input).float()
         bpd_var = torch.autograd.Variable(bpd).float()
         depth_var = torch.autograd.Variable(depth).float()
         # print(input_var.size())
+        # print(bpd_var.size())
         # print(depth_var.size())
         # h
 
@@ -496,7 +497,7 @@ def validate(segmenter, val_loader, epoch, num_classes=-1):
         for i, sample in enumerate(val_loader):
             input = sample["image"]
             bpd = sample["bpd"].unsqueeze(1)
-            depth = sample["depth"]
+            depth = sample["depth"].unsqueeze(1)
             target = sample["mask"]
             input_var = torch.autograd.Variable(input).float().cuda()
             bpd_var = torch.autograd.Variable(bpd).float().cuda()
