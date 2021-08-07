0705fd0b0f82c037ff016636b28a8676deb6d34f
Elham-gh <76242088+Elham-gh@users.noreply.github.com>
2021-08-08 02:11:00 +0430

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 # DATASET PARAMETERS
-TRAIN_DIR = '/content/drive/MyDrive/datasets/nyudv2/'
-VAL_DIR = TRAIN_DIR
-TRAIN_LIST = ['/content/light-weight-refinenet/data/train_CEN.nyu'] * 6
-VAL_LIST = ['/content/light-weight-refinenet/data/val_CEN.nyu'] * 6
-BPD_DIR = ['/content/drive/MyDrive/datasets/nyu/CEN_bpds.pkl'] * 6
+TRAIN_DIR = '/content/drive/MyDrive/datasets/sunrgbd/train/'
+VAL_DIR = '/content/drive/MyDrive/datasets/sunrgbd/val/'
+TRAIN_LIST = ['/content/drive/MyDrive/datasets/sunrgbd/train/train.txt'] * 6
+VAL_LIST = ['/content/drive/MyDrive/datasets/sunrgbd/val/val.txt'] * 6
+BPD_DIR_TR = ['/content/drive/MyDrive/SuperBPD/train_sun.pkl'] * 6
+BPD_DIR_VL = ['/content/drive/MyDrive/SuperBPD/sun_test_bpds.pkl'] * 6
 SHORTER_SIDE = [350] * 6
 CROP_SIZE = [500] * 6
 NORMALISE_PARAMS = [
@@ -31,16 +32,16 @@ FREEZE_BN = [True] * 1
 NUM_SEGM_EPOCHS = [100] * 6
 PRINT_EVERY = 10
 RANDOM_SEED = 42
-CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR/ckpt_att/'
+CKPT_PATH = '/content/drive/MyDrive/Super-BPD/LWR_sun/ckpt_att/'
 VAL_EVERY = [10] * 6  # how often to record validation scores
 RESUME = '' #CKPT_PATH
 
 # OPTIMISERS' PARAMETERS
-LR_ENC = [1e-3, 5e-4, 2.5e-4, 1e-4]  # TO FREEZE, PUT 0
-LR_DEC = [1e-2, 5e-3, 2.5e-3, 1e-3]
+LR_ENC = [5e-4, 2.5e-4, 1e-4, 5e-5]  # TO FREEZE, PUT 0
+LR_DEC = [5e-3, 2.5e-3, 1e-3, 5e-4]
 
-MOM_ENC = [0] * 6  # TO FREEZE, PUT 0
-MOM_DEC = [0] * 6
+MOM_ENC = [.9] * 6  # TO FREEZE, PUT 0
+MOM_DEC = [.9] * 6
 WD_ENC = [1e-5] * 6  # TO FREEZE, PUT 0
 WD_DEC = [1e-5] * 6
 OPTIM_DEC = "sgd"
